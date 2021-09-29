4a20bca0a89095fec5d7fca4a5629a40b34309d9
Rick <ricksmycodeguy@gmail.com>
2021-09-29 17:43:39 -0500

    validateUserId wired to all /:id routes

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const User = require('../users/users-model')
 
 
 function logger(req, res, next) {
@@ -9,10 +10,18 @@ function logger(req, res, next) {
   next()
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
-  console.log('validateUser middleware')
-  next()
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id)
+    if (!user) {
+      res.status(404).json({ message: 'user not found' })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'catch: problem finding user' })
+  }
 }
 
 function validateUser(req, res, next) {
