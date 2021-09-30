8ed9d5b3536df7168b9cf1aad85f3a28a2ef956c
Rick <ricksmycodeguy@gmail.com>
2021-09-30 09:36:21 -0500

    refactor delete to return user & no message

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -12,43 +12,54 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   User.get()
-  .then(users => {
-    res.json(users)
-  })
-  .catch(next)
+    .then(users => {
+      res.json(users)
+    })
+    .catch(next)
 });
 
 router.get('/:id', validateUserId, (req, res) => {
-res.json(req.user)
+  res.json(req.user)
 });
 
 router.post('/', validateUser, (req, res, next) => {
-  User.insert({ name: req.name})
-  .then(newUser => {
-    //throw new Error('testing')
-    res.status(201).json(newUser)
-  })
-  .catch(next)
+  User.insert({ name: req.name })
+    .then(newUser => {
+      //throw new Error('testing')
+      res.status(201).json(newUser)
+    })
+    .catch(next)
 });
 
 router.put('/:id', validateUserId, validateUser, (req, res, next) => {
   User.update(req.params.id, { name: req.name })
-  .then(() => {
-    return User.getById(req.params.id)
-  })
-  .then(user => {
-    res.status(200).json(user)
-  })
-  .catch(next)
+    .then(() => {
+      return User.getById(req.params.id)
+    })
+    .then(user => {
+      res.status(200).json(user)
+    })
+    .catch(next)
 });
 
 router.delete('/:id', validateUserId, (req, res, next) => {
-User.remove(req.params.id)
-.then(() => {
-  res.status(200).json({ message: "User removed by magic"})
-})
-.catch(next)
+  User.remove(req.params.id)
+    .then(() => {
+      res.status(200).json(
+        req.user
+      )
+    })
+    .catch(next)
 });
+//Alternative delete 
+// router.delete('/:id', validateUserId, async (req, res, next) => {
+//   try{
+//     await User.remove(req.params.id)
+//     res.status(200).json(req.user)
+//   } catch (err) {
+//     next(err)
+//   }
+// });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
@@ -67,7 +78,7 @@ router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
 router.use((err, req, res, next) => {//eslint-disable-line
   res.status(err.status || 500).json({
     customerMessage: "Something tragic happened inside posts router ",
-    message: err.message, 
+    message: err.message,
     stack: err.stack,
   })
 })
